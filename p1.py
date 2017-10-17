20c6ea48b53a070724ece79fc7a4950117a8cd98
Yeray <yeraymendez07@gmail.com>
2017-10-18 01:23:00 +0200

    convolve cuasi listo, falla con algunos valores

--- p1.py
+++ p1.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import functools 
-import matplotlib
 
 #----To show an image
 showingImage = functools.partial(plt.imshow,vmin = 0, vmax = 255, cmap= plt.get_cmap('gray'))
@@ -50,11 +49,10 @@ def histEnhance(inputImage,cenValue,winSize):
 def compareImages(inputImage,outputImage):
     fig = plt.figure()
     a=fig.add_subplot(1,2,1)
-    imgplot = plt.imshow(loadImage(inputImage))
+    showingImage(loadImage(inputImage))
     a.set_title('Before')
     a=fig.add_subplot(1,2,2)
     showingImage(outputImage)
-    imgplot.set_clim(0.0,0.7)
     a.set_title('After')
     plt.show()
 
@@ -94,12 +92,71 @@ def testHistAdapt(inputImage,minValue,maxValue):
     compareHist(inputImage,histAdapt(inputImage,minValue,maxValue))
 #-------------------------------------------------#
 
-#----------------------Tests----------------------#
+#--Spatial filtering: Smoothing and highlighting--#
 
-#testWindowLevelContrastEnhancement('lena.bmp',100,20)
-#testHistAdapt('lena.bmp',0,255)
+def createKernel(maxValue,rows,cols):
+    m = np.ones((rows,cols),dtype=int)
+    m[int(rows/2),int(cols/2)] = maxValue
+    return m
+
+def convolutionFunction(pixelImage,kernel):
+    rowsImage,colsImage = pixelImage.shape
+    rows,cols = kernel.shape
+    rowsKernel = rows -1
+    colsKernel = cols -1
+    limitRow = rowsImage - int(rowsKernel/2) 
+    endLimitRow = rowsImage
+    limitCol = colsImage - int(colsKernel/2) 
+    endLimitCol = colsImage
+    if int(rowsKernel/2)==1 :
+        endInitRow = 1
+    else:
+        endInitRow = int((rowsKernel/2))
+    if int(colsKernel/2) == 1:
+        endInitCol = 1
+    else:
+        endInitCol = int(colsKernel/2)
+    aux = np.zeros((rowsImage,colsImage),dtype=int)
+    for (x,y), value in np.ndenumerate(pixelImage):
+        if (x < endInitRow or y < endInitCol) or ((x >= limitRow and x < endLimitRow) or (y >= limitCol and y < endLimitCol)):
+            aux[x,y] = value
+        else:
+            #print(x,y) 
+            q = -1
+            q1 = rows
+            i =  -1 + x-endInitRow
+            i2 = x + endInitRow 
+            convolutionValue = 0
+            while i < i2 and q < q1:
+                i = i +1
+                q = q +1
+                z = 0 
+                z1 = cols
+                j = y - endInitCol
+                j2 = y + endInitCol +1
+                while j < j2  and z < z1 : 
+                    count = pixelImage[i,j]*kernel[q,z]   
+                    convolutionValue = convolutionValue + count
+                    '''print('PIXELIMAGE : ',i,j)
+                    print('KERNEL : ',q,z)'''
+                    j = j +1
+                    z = z+1
+            aux[x,y] = int(convolutionValue/10)
+    aux = aux.astype(np.uint8)
+    return aux
+
+def convolve(inputImage,kernel):
+    return convolutionFunction(loadImage(inputImage),kernel)
+    
+def testConvolve(inputImage,kernel):
+    compareImages(inputImage,convolve(inputImage,kernel))
 
 #-------------------------------------------------#
 
+#----------------------Tests----------------------#
 
+#testWindowLevelContrastEnhancement('lena_gray.bmp',100,20)
+#testHistAdapt('lena_gray.bmp',100,200)
+testConvolve('lena_gray.bmp',createKernel(2,3,3))
 
+#-------------------------------------------------#
